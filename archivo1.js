68bfbe37713f141b0d85d3a77a9ed643fedac835
quirogacamilodev <quirogacamilodev@gmail.com>
2023-04-04 14:15:03 -0300

    first commit

---
+++ archivo1.js
@@ -0,0 +1,9 @@
+// Elementos basicos
+
+// Datos: Numericos, cadena, booleanos.
+// Variables: Globales, locales.
+// Objetos: Array, booleanos, date, math, string
+// Array: Indice, longuitud.
+// Funciones: Argumentos.
+
+// Control de flujo:
